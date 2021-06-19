c084abc18149c3793bab00c0e4214c0cc05a524b
Nikolay <kolekdoma@mail.ru>
2021-06-19 19:13:07 +0300

    Добавил кнопки

--- PhotoTimer_WindowsFormsApp/PhotoTimer_WindowsFormsApp/MainForm.cs
+++ PhotoTimer_WindowsFormsApp/PhotoTimer_WindowsFormsApp/MainForm.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -25,7 +27,8 @@ namespace PhotoTimer_WindowsFormsApp
 
         private void startButton_Click(object sender, EventArgs e)
         {
-
+            TimerCallback tm = new TimerCallback(MakePhoto);
+            System.Threading.Timer timer = new System.Threading.Timer(tm, 0, 1000, 3000);
         }
 
         private void pauseButton_Click(object sender, EventArgs e)
@@ -35,21 +38,95 @@ namespace PhotoTimer_WindowsFormsApp
 
         private void showButton_Click(object sender, EventArgs e)
         {
-
+            System.Diagnostics.Process.Start(path);
         }
 
         private void resetButton_Click(object sender, EventArgs e)
         {
+            DeleteFolder(subpath);
 
+            Application.Restart();
         }
 
         private void deleteButton_Click(object sender, EventArgs e)
         {
+            DialogResult warning = MessageBox.Show($"Вы действительно хотите удалить папку {nameFolder} со всем содержимым?", "Удаление объекта", MessageBoxButtons.YesNo);
 
+            if (warning == DialogResult.Yes)
+
+            {
+
+                DeleteFolder(path);
+
+            }
+
+
+
+            else if (warning == DialogResult.No)
+
+            {
+
+                warning = DialogResult.Cancel;
+
+            }
         }
 
         private void countLabel_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private static void DeleteFolder(string path)
+
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(path);
+                dirInfo.Delete(true); // папку надо удалять со всем содержимым
+                MessageBox.Show($"Папка \"{nameFolder}\" успешно удалена.");
+            }
+
+            catch (Exception ex)
+
+            {
+
+                Console.WriteLine(ex.Message);
+
+            }
+
+        }
+        public static void MakePhoto(object obj)
+
+        {
+
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+
+            dirInfo.Create();
+
+
+            dirInfo.CreateSubdirectory(subpath);
+
+            var instance = new PhotoView();
+
+            instance.myEvent += Instance_myEvent;
+
+            instance.InvokeEvent();
+
+            Bitmap printscreen = new Bitmap(100, 100);
+
+            Graphics graphics = Graphics.FromImage(printscreen as Image);
+
+            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+
+            printscreen.Save($@"C:\Program Files\TestFolder\{subpath}\printscreen {DateTime.Now.ToString("(hh.mm.ss)")}.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+        }
+
+        private static void Instance_myEvent()
+
+        {
+
+            MessageBox.Show("В папку добавлено новое фото.");
 
         }
     }
